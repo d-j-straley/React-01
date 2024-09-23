d0754fa6c562b2998200525b664ac8fec073b06b
David J. Straley <djstraley@sandyhook.com>
2024-09-22 21:25:14 -0500

    Add project files.

---
+++ react-01/src/main/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Header from './Header'
+
+function App() {
+  return (
+    <div className='container'>
+      <Header subtitle='Worldwide Residential Multiple Listing Service' />
+    </div>
+  );
+}
+
+export default App
